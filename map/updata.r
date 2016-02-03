84ffaf94c416d55703531d399769308ba525fd90
Bruce <nxsyy55@gmail.com>
2016-02-03 17:59:14 +0800

    地区热力图
    上海热力图解决

--- map/updata.r
+++ map/updata.r
@@ -13,3 +13,11 @@ number <- toString(dat2[, 6])
 str <- paste("var long = [", long, "];", "var lat = [", lat, "];", 
              "var numbers = [", number, "];")
 write(str, file = 'data20.js')
+
+dat2 <- read_csv("finalResult10.csv")
+long <- toString(dat2[, 7])
+lat <- toString(dat2[, 8])
+number <- toString(dat2[, 6])
+str <- paste("var long = [", long, "];", "var lat = [", lat, "];", 
+             "var numbers = [", number, "];")
+write(str, file = 'data.js')
